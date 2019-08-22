04c34659ac0dcf5ecafd92a503a5fa2e7cbf6063
Kirill <54350762+Zur-Ciro@users.noreply.github.com>
2019-08-22 16:05:25 +0500

    Add files via upload
    Сорти

---
+++ Сортировка.py
@@ -0,0 +1,10 @@
+a = [1,3,89,1.32,49,23]
+a = sorted (a)
+print(a)
+a = sorted (a,reverse = True) # sorted создает новую последовательность
+print (a)
+list.sort(a) #sort - меняет старый список
+print(a)
+list.sort(a,reverse = True)
+print(a)
+input()
